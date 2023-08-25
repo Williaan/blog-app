212dee001677970d8fb7165e3e880efea7ed1adf
Willian <willianmarcos907@gmail.com>
2023-08-25 08:05:42 -0300

    add link api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://api-blog-post.vercel.app/',
+    baseURL: 'https://api-blog-post.onrender.com/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
